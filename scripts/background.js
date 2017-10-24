61d753e577cbae848d225c7079bafa5e14aae3d3
胡昕宇 <michaelhu@koxinyutekiMacBook-Air.local>
2017-10-25 03:24:59 +0800

    internet detection works now

--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,5 @@
 
 window.setInterval(() => {
-  console.log("first time out");
-  setTimeout(() => {
-    chrome.browserAction.setIcon({path: "offline.png"});
-  }, 1000);
-  console.log("first time out over")
-
-  console.log("icon changed to red")
-  console.log("second time out");
-  setTimeout(() => {
-    chrome.browserAction.setIcon({path: "online.png"});
-  }, 1000);
-  console.log("second time out over");
-  console.log("icon changed to green")
-}, 3000);
+  let condition = navigator.onLine ? "online" : "offline";
+  chrome.browserAction.setIcon({path: condition + ".png"});
+}, 2000);
