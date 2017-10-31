fba8c469dce25db430d763f3fddce3558142d58f
胡昕宇 <michaelhu@koxinyutekiMacBook-Air.local>
2017-11-01 01:29:55 +0800

    a

--- scripts/background.js
+++ scripts/background.js
@@ -2,6 +2,6 @@ var condition;
 
 window.setInterval(() => {
   condition = navigator.onLine ? "online" : "offline";
-  localStorage.setItem("current_status", JSON.stringify(condition));
+  //localStorage.setItem("current_status", JSON.stringify(condition));
   chrome.browserAction.setIcon({path: condition + ".png"});
-}, 2000);
+}, 1000);
