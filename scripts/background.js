6ab246d91b4f1ee496b828822ab39ea0ee01c122
胡昕宇 <michaelhu@koxinyutekiMacBook-Air.local>
2017-11-10 02:47:25 +0800

    yellow light added

--- scripts/background.js
+++ scripts/background.js
@@ -2,29 +2,37 @@ var condition;
 var trueCondition;
 var flag = true;
 
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() {
+            if (anHttpRequest.readyState == 4)
+                aCallback(anHttpRequest.status);
+        }
+        anHttpRequest.open( "GET", aUrl, true );
+        anHttpRequest.send();
+    }
+}
+var client = new HttpClient();
 window.setInterval(() => {
   condition = navigator.onLine ? "online" : "offline";
   //localStorage.setItem("current_status", JSON.stringify(condition));
-  /*
-  let xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    console.log(xmlHttp.status);
-      if (xmlHttp.status === 200){
-        flag = true;
-      } else {
-        flag = false;
-      }
-  }
-  xmlHttp.open("GET", "umich.edu", true); // true for asynchronous
-  if (condition === "offline") {
+
+  client.get('http://www.google.com', function(response) {
+    if (response == 200) {
+      flag = true;
+    } else {
+      flag = false;
+    }
+  });
+  if (condition == "offline") {
     trueCondition = "offline";
   } else {
-    if (flag === true) {
+    if (flag == true) {
       trueCondition = "online";
     } else {
       trueCondition = "yellow";
     }
   }
-  */
-  chrome.browserAction.setIcon({path: condition + ".png"});
+  chrome.browserAction.setIcon({path: trueCondition + ".png"});
 }, 1000);
